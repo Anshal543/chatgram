c18b05e86e6e59835448cb933f940252426b3f2d
Anshal543 <sherhasan543@gmail.com>
2024-06-21 01:27:03 +0500

    chat apis

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -113,3 +113,72 @@ export const addToGroup = asyncHandler(async (req, res) => {
     if (!addedToChat) throw customError("User could not be added to chat", 500);
     res.send(addedToChat);
 });
+
+export const removeUserFromGroup = asyncHandler(async (req, res) => {
+    const { chatId, userId } = req.body;
+    const removedFromChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            $pull: { users: userId },
+        },
+        { new: true }
+    )
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+
+    if (!removedFromChat)
+        throw customError("User could not be removed from chat", 500);
+    res.send(removedFromChat);
+});
+
+// export const leaveGroup = asyncHandler(async (req, res) => {
+//     const { chatId } = req.body;
+//     const leftGroup = await Chat.findByIdAndUpdate(
+//         chatId,
+//         {
+//             $pull: { users: req.user._id },
+//         },
+//         { new: true }
+//     )
+//         .populate("users", "-password")
+//         .populate("groupAdmin", "-password");
+
+//     if (!leftGroup) throw customError("Could not leave group", 500);
+//     res.send(leftGroup);
+// });
+
+export const leaveGroup = asyncHandler(async (req, res) => {
+    const { chatId } = req.body;
+
+    const chat = await Chat.findById(chatId);
+
+    if (!chat) throw customError("Group not found", 404);
+
+    const isAdmin = chat.groupAdmin === req.user._id;
+
+    if (isAdmin) {
+        const remainingUsers = chat.users.filter(userId => userId !== req.user._id);
+
+        if (remainingUsers.length === 0) {
+            await Chat.findByIdAndDelete(chatId);
+            return res.send({ message: "Group deleted as no users are left" });
+        } else {
+            chat.groupAdmin = remainingUsers[0];
+        }
+    }
+
+    const leftGroup = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            $pull: { users: req.user._id },
+            groupAdmin: chat.groupAdmin
+        },
+        { new: true }
+    )
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+
+    if (!leftGroup) throw customError("Could not leave group", 500);
+
+    res.send(leftGroup);
+});
--- backend/src/routes/chat.route.js
+++ backend/src/routes/chat.route.js
@@ -5,6 +5,8 @@ import {
     createGroupChat,
     renameGroup,
     addToGroup,
+    removeUserFromGroup,
+    leaveGroup
 } from "../controllers/chat.controller.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
@@ -15,5 +17,7 @@ router.route("/").post(verifyToken, accessChat);
 router.route("/group").post(verifyToken, createGroupChat);
 router.route("/group/rename").put(verifyToken, renameGroup);
 router.route("/group/add").put(verifyToken, addToGroup);
+router.route("/group/remove").put(verifyToken, removeUserFromGroup);
+router.route("/group/leave").put(verifyToken, leaveGroup);
 
 export default router;
